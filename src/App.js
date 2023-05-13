cd88539b64b58f9141d7f2acf7fb64bfeed2cde3
mhd-atheel <aathilmazz1234@gmail.com>
2023-05-13 15:32:37 +0530

    >>>>>>>>adding the dashborad donations leaves students teacher screens
    >>>>>> adding react-router-dom
    
    >>>>> adding routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-
-import './App.css'
-import Sidebar from './components/Sidebar';
-
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import Dashboard from "./screens/Dashboard";
+import Students from "./screens/Students";
+import Teacher from "./screens/Teacher";
 
 function App() {
-
-
-
-return (
-    <div className='App'>
-     
-      <div className='row'>
-        
-        <Sidebar/>
-      
-    
-          <h1> Hello Atheel</h1>
-       
-      
+  return (
+    <div className="App">
+      <div className="row">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="about" element={<Students />} />
+          <Route path="contact" element={<Teacher />} />
+        </Routes>
       </div>
     </div>
   );
